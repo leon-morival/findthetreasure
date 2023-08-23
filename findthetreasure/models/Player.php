1cf958fc4e2a14ad2137c726782a33886d5839c8
Grégory <87112706+Saimoen@users.noreply.github.com>
2023-08-23 15:49:03 +1100

    Add files via upload

---
+++ findthetreasure/models/Player.php
@@ -0,0 +1,88 @@
+<?php
+require_once 'Chest.php';
+require_once 'Monster.php'; // Assurez-vous d'inclure correctement la classe Monster
+
+class Player
+{
+
+    private $chestX;
+    private $positionX;
+    private $positionY;
+    private $xp;
+    private $power;
+    private $pv;
+
+    public function __construct()
+    {
+
+        $this->positionX = rand(0, 20);
+        $this->positionY = rand(0, 20);
+        if (isset($_SESSION['playerPositionX'])) {
+            $this->positionX = $_SESSION['playerPositionX'];
+            $this->positionY = $_SESSION['playerPositionY'];
+        } else {
+            // Set initial position
+            $this->positionX = rand(0, 20);
+            $this->positionY = rand(0, 20);
+            $_SESSION['playerPositionX'] = $this->positionX;
+            $_SESSION['playerPositionY'] = $this->positionY;
+        }
+         $this->xp = 0;
+         $this->power = rand(50, 100);
+         $this->pv = rand(50,100);
+
+    }
+
+
+    public function move($direction)
+    {
+        switch ($direction) {
+            case 0:
+
+                if ($this->positionY < 20) {
+                    $this->positionY++;
+                }
+
+                break;
+            case 1:
+                if ($this->positionX < 20) {
+                    $this->positionX++;
+                }
+                break;
+            case 2:
+                if ($this->positionY > 0) {
+                    $this->positionY--;
+                }
+                break;
+            case 3:
+                if ($this->positionX > 0) {
+                    $this->positionX--;
+                }
+                break;
+        }
+        $_SESSION['playerPositionX'] = $this->positionX;
+        $_SESSION['playerPositionY'] = $this->positionY;
+    }
+
+    function getPositionX()
+    {
+        return  $this->positionX;
+    }
+
+    function getPositionY()
+    {
+        return $this->positionY;
+    }
+
+    function getPV()
+    {
+        return $this->pv;
+    }
+    function getPower()
+    {
+        return $this->power;
+    }
+
+}
+
+// Usage
