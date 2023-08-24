68fc09cac50b4396f3fd0564dd76f3d8d170b515
Colin <112839508+Colinsoriano@users.noreply.github.com>
2023-08-24 11:40:56 +1100

    Update controller.php
    Texte en français

--- controllers/controller.php
+++ controllers/controller.php
@@ -2,30 +2,64 @@
 session_start();
 require_once '../models/Player.php';
 require_once '../models/Monster.php';
+require_once '../models/Fight.php';
 $monster = new Monster();
 $player = new Player();
 $chest = new Chest();
+
 $monster->getMonsters();
 // var_dump($monster->getMonsters());
+$playerX = $player->getPositionX();
+$playerY = $player->getPositionY();
+
+$chestX = $chest->getPositionX();
+$chestY = $chest->getPositionY();
+
+// Compare positions and display message if they are the same
+// header('Location: ' . $_SERVER['PHP_SELF']);
+function findChest()
+{
+    global $playerX;
+    global $playerY;
+    global $chestX;
+    global $chestY;
+    if ($playerX === $chestX && $playerY === $chestY) {
+        echo "Félicitations ! <br>Vous avez trouvé le trésor !";
+        echo '<br>';
+        echo "Position du joueur : " . $playerX . ' ' . $playerY;
+        echo '<br>';
+        echo "Position du trésor : " . $chestX . ' ' . $chestY;
+    } else {
+        echo "Continuez l'exploration...";
+    }
+}
+
+
+$fight = new Fight();
 // Process user input to move the player
 if (isset($_GET['direction'])) {
     $direction = (int)$_GET['direction'];
     $player->move($direction);
 
-    // Imprimez le tableau de monstres pour vérifier
+    foreach ($monster->getMonsters() as $key => $monster) {
+        if ($player->getPositionX() == $monster['positionX'] && $player->getPositionY() == $monster['positionY']) {
+            // Commencez le combat entre le joueur et le monstre
+            $result = $fight->startFight($player, $monster);
 
-}
-if (isset($_GET['reset']) && $_GET['reset'] === 'true') {
-    // Reset the game by generating new values and clearing the session data
-    $player = new Player();
-    $monster = new Monster();
-    $chest = new Chest();
+            // Vous pouvez traiter le résultat du combat ici, comme ajuster les points de vie du joueur et du monstre, etc.
 
+
+            // Retirez le monstre de la liste s'il a été vaincu ou autre action nécessaire
+            unset($_SESSION['monsters'][$key]);
+        }
+    }
+} elseif (isset($_GET['reset']) && $_GET['reset'] === 'true') {
     session_unset(); // Clear all session variables
     session_destroy(); // Destroy the session
     session_start(); // Start a new session
-
+    header('Location: ' . $_SERVER['PHP_SELF']);
 }
+
 
 
 // Include the view
