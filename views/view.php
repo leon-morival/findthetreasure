e3f712ab91ebc5992867948ea805ee7d355cf477
Colin <112839508+Colinsoriano@users.noreply.github.com>
2023-08-24 11:41:29 +1100

    Update view.php

--- views/view.php
+++ views/view.php
@@ -1,4 +1,13 @@
-<?php require_once '../controllers/controller.php'; ?>
+<?php
+require_once '../controllers/controller.php';
+require_once '../models/Player.php';
+require_once '../models/Monster.php';
+require_once '../models/Chest.php';
+
+$player = new Player();
+$monster = new Monster();
+$chest = new Chest();
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,47 +17,19 @@
     <title>Monster Game</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <style>
-        /* Add your CSS styles here */
-        .game-board {
-            display: grid;
-            grid-template-columns: repeat(20, 30px);
-            grid-template-rows: repeat(20, 30px);
-            gap: 1px;
-        }
-
-        .cell {
-            width: 30px;
-            height: 30px;
-            border: 1px solid black;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            font-size: 1.2rem;
-        }
-
-        .player {
-            background-color: white;
-            color: white;
-        }
-
-        .monster {
-            background-color: white;
-            color: white;
-        }
-
-        .chest {
-            background-color: white;
-            color: white;
-        }
-    </style>
+    <link rel="stylesheet" href="../public\css\style.css">
 </head>
 
 <body>
 
-    <div style="text-align: center;">
-        <a href="../index.php"><img src="../public/assets/bandeau.jpg" alt="Logo"></a>
-    </div>
+    <!-- <div class="row">
+        <div class="col-8">
+            <div class="text-center">
+                <a href="../index.php"><img src="../public/assets/bandeau.jpg" alt="Logo" class="w-100"></a>
+            </div>
+        </div>
+    </div> -->
+
     <div class="container text-center">
         <div class="header">
             <h1 class="text-primary" style="padding-top: 20px; text-align: center;">Find the Treasure</h1>
@@ -56,29 +37,31 @@
         <hr class="border border-primary border-3 opacity-75">
 
         <div class="row">
-            <!-- Game board -->
             <div class="col-md-9">
                 <div class="game-board">
                     <?php
-                    // Assuming $player, $monster, and $chest are already initialized
-
-                    // Create a 20x20 game board
                     $boardSize = 21;
                     for ($row = 1; $row < $boardSize; $row++) {
                         for ($col = 1; $col < $boardSize; $col++) {
                             echo '<div class="cell';
-
-                            // Check if the current position contains the player, monster, or chest
                             if ($player->getPositionX() === $col && $player->getPositionY() === $row) {
                                 echo ' player"><img src="../public/assets/sword_player.png">';
                             } elseif ($chest->getPositionX() === $col && $chest->getPositionY() === $row) {
                                 echo ' chest"><img src="../public/assets/golden_treasure.png">';
-                            } elseif (in_array([$col, $row], $monster->getMonsters(), true)) {
-                                echo ' monster"><img style="border-radius:50%;" src="../public/assets/monster.png">';
                             } else {
-                                echo '">';
+                                $isMonsterHere = false;
+                                foreach ($monster->getMonsters() as $monsterData) {
+                                    if ($monsterData["positionX"] === $col && $monsterData["positionY"] === $row) {
+                                        $isMonsterHere = true;
+                                        break;
+                                    }
+                                }
+                                if ($isMonsterHere) {
+                                    echo ' monster"><img src="../public/assets/monster.png">';
+                                } else {
+                                    echo '">';
+                                }
                             }
-
                             echo '</div>';
                         }
                     }
@@ -86,18 +69,15 @@
                 </div>
             </div>
 
-            <!-- Player movement buttons -->
-            <div class="col-md-3" style="right:90px;">
+            <div class="col-md-3" style="right: 90px;">
                 <p>Position du joueur : (<?php echo $player->getPositionX(); ?>, <?php echo $player->getPositionY(); ?>)</p>
-
                 <p>Position du coffre : (<?php echo $chest->getPositionX(); ?>, <?php echo $chest->getPositionY(); ?>)</p>
-
-                <p>Déplacez-vous</p>
+                <p>Déplacez-vous sur la carte :</p>
                 <div class="w-100 mx-auto text-center">
                     <div class="row">
                         <div class="col-4"></div>
                         <div class="col-4">
-                            <a href="../controllers/controller.php?direction=2"><i class="fa-solid fa-arrow-up fa-3x"></i></a>
+                            <a href="../controllers/controller.php?direction=0"><i class="fa-solid fa-arrow-up fa-3x"></i></a>
                         </div>
                         <div class="col-4"></div>
                     </div>
@@ -113,21 +93,36 @@
                     <div class="row">
                         <div class="col-4"></div>
                         <div class="col-4">
-                            <a href="../controllers/controller.php?direction=0"><i class="fa-solid fa-arrow-down fa-3x"></i></a>
+                            <a href="../controllers/controller.php?direction=2"><i class="fa-solid fa-arrow-down fa-3x"></i></a>
                         </div>
                         <div class="col-4"></div>
                     </div>
                 </div>
 
-                <!-- Reset button -->
+                <div class="row">
+                    <div class="col-md-12 text-center mt-4" id="carac">
+                        <i class="fa-solid fa-heart fa-2x" style="color: red;">&nbsp;<?= $player->getPV() ?></i>
+                        <br />
+                        <br />
+                        <i class="fa-solid fa-bolt fa-2x" style="color: orange;">&nbsp;<?= $player->getPower() ?></i>
+                        <br />
+                        <br />
+                        <i class="fa-solid fa-2x" style="color: blue;">XP&nbsp;<?= $player->getXp() ?></i>
+                    </div>
+                </div>
+                <br />
+                <div class="border border-rounded p-3" style="width: 100%;">
+                    <?= findChest() ?>
+                </div>
                 <div class="w-100 mx-auto text-center mt-3">
-                    <a class="btn btn-danger" href="../controllers/controller.php?reset=true">Reset Game</a>
+                    <a class="btn btn-danger" href="../controllers/controller.php?reset=true">Nouvelle Partie</a>
+                </div>
+
+                <div class="w-100 mx-auto text-center mt-3">
+                    <a class="btn btn-primary" href="../index.php">Retour à l'accueil</a>
                 </div>
             </div>
         </div>
-    </div>
-    <div style="text-align: center; padding-top: 20px;">
-        <img src="../public/assets/footer.jpg" alt="Logo">
     </div>
 </body>
 
