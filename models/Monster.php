f622fc834c1bf9eb3bbf36e3b1adee58c00aa75e
Colin <112839508+Colinsoriano@users.noreply.github.com>
2023-08-24 15:01:08 +1100

    Update Monster.php
    Revalorisation des points de FORCE et HP du joueur pour plus de cohérence

--- models/Monster.php
+++ models/Monster.php
@@ -23,8 +23,8 @@ class Monster
         while (count($this->monsterArray) < $this->arraySize) {
             $this->x = rand(0, 20);
             $this->y = rand(0, 20);
-            $this->pv = rand(60, 80);
-            $this->force = rand(90, 100);
+            $this->pv = rand(20, 100);
+            $this->force = rand(20, 50);
             $point = ["positionX" => $this->x, "positionY" => $this->y, "PV" => $this->pv, "Force" => $this->force];
 
             if (!$this->isDuplicate($point)) {
